d4a3ccd894a3917e0417f6aa15d03d3e6771ffda
vagrant <vagrant@homestead>
2023-08-03 13:27:42 +0000

    Initial commit

---
+++ app/Console/Commands/ParsIndexLinksCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class ParsIndexLinksCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'pars:index';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $response = Http::get('https://iamtex.com.ua');
+        print($response);
+    }
+}
