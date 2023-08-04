ea7adc18abe17ec80f7657b824ccaf1e4a341ca5
ArtRyzen <artembabichev@gmail.com>
2023-08-04 15:49:39 +0300

    data work, migration

---
+++ app/Console/ParseData/ProductParserObject.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Console\ParseData;
+
+class ProductParserObject
+{
+
+}
---
+++ database/migrations/2023_02_07_032928_create_product_category_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     * https://youtu.be/c0yuY_Ugacg?list=PLd2_Os8Cj3t8pnG4ubQemoqnTwf0VFEtU&t=283
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_category', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained();
+            $table->foreignId('category_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_category');
+    }
+};
